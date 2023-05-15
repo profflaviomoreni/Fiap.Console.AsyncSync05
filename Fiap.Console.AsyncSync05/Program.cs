8fb0f8df382ad718f1d3d6f22f495237556da05d
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-05-14 22:28:57 -0300

    tudo async com espera sem funcionar

--- Fiap.Console.AsyncSync05/Program.cs
+++ Fiap.Console.AsyncSync05/Program.cs
@@ -15,29 +15,45 @@ class MyClass
 
     public static async Task print1()
     {
+        Task.Delay(GenerateRandom());
         Console.WriteLine("1.1");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("1.2");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("1.3");
     }
 
     public static async Task print2()
     {
+        Task.Delay(GenerateRandom());
         Console.WriteLine("2.1");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("2.2");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("2.3");
     }
 
     public static async Task print3()
     {
+        Task.Delay(GenerateRandom());
         Console.WriteLine("3.1");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("3.2");
 
+        Task.Delay(GenerateRandom());
         Console.WriteLine("3.3");
+    }
+
+
+    static int GenerateRandom()
+    {
+        Random random = new Random();
+        return random.Next(300, 2700);
     }
 
 }
