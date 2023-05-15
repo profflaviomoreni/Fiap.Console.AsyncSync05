0f6ebaf5b87cfb9a42aa64c2801a92119113a48e
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-05-14 22:23:27 -0300

    Add project files.

---
+++ Fiap.Console.AsyncSync05/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+class MyClass
+{
+
+    public static async Task Main(String[] args)
+    {
+
+        print1();
+        print2();
+        print3();
+
+        Console.ReadKey();
+    }
+
+    public static void print1()
+    {
+        Console.WriteLine("1.1");
+
+        Console.WriteLine("1.2");
+
+        Console.WriteLine("1.3");
+    }
+
+    public static void  print2()
+    {
+        Console.WriteLine("2.1");
+
+        Console.WriteLine("2.2");
+
+        Console.WriteLine("2.3");
+    }
+
+    public static void print3()
+    {
+        Console.WriteLine("3.1");
+
+        Console.WriteLine("3.2");
+
+        Console.WriteLine("3.3");
+    }
+
+}
